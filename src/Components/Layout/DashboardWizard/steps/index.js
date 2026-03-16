b3387b8076099c04706f95e41e1000074d70d1d7
John Giatropoulos <johngiatropoulos@Johns-Mac-mini.local>
2026-03-16 18:49:33 -0400

    feat(DASH-112): WizardCustomizeStep with name, folder, theme pickers and dual-path create logic

--- src/Components/Layout/DashboardWizard/steps/index.js
+++ src/Components/Layout/DashboardWizard/steps/index.js
@@ -2,3 +2,4 @@ export { WizardIntentStep } from "./WizardIntentStep";
 export { WizardProvidersStep } from "./WizardProvidersStep";
 export { WizardResultsStep } from "./WizardResultsStep";
 export { WizardLayoutPreviewStep } from "./WizardLayoutPreviewStep";
+export { WizardCustomizeStep } from "./WizardCustomizeStep";
